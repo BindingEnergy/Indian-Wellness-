8b174c4a4c3d71296a4fc4a23e35c775c1889341
BindingEnergy <santusht.matra11@hotmail.com>
2024-02-23 18:31:01 +0530

    added soothing sound to play cintunuously while timer is on

--- meditation.js
+++ meditation.js
@@ -6,21 +6,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const timer5Btn = document.getElementById('timer5');
     const timer10Btn = document.getElementById('timer10');
     const timer15Btn = document.getElementById('timer15');
-    
+
     const startBtn = document.getElementById('startBtn');
     const stopBtn = document.getElementById('stopBtn');
     const pauseBtn = document.getElementById('pauseBtn');
     const restartBtn = document.getElementById('restartBtn');
-    
+
     // Add click event listeners to the timer buttons
     timer5Btn.addEventListener('click', function () {
         startTimer(5 * 60); // 5 minutes in seconds
     });
-    
+
     timer10Btn.addEventListener('click', function () {
         startTimer(10 * 60); // 10 minutes in seconds
     });
-    
+
     timer15Btn.addEventListener('click', function () {
         startTimer(15 * 60); // 15 minutes in seconds
     });
@@ -38,9 +38,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     restartBtn.addEventListener('click', function () {
-        startTimer(duration);
+        restartTimer();
     });
-    
+
     // Function to start the timer
     function startTimer(timeInSeconds) {
         stopTimer(); // Stop any existing timers
@@ -89,6 +89,12 @@ document.addEventListener('DOMContentLoaded', function () {
         // Pause soothing sound
         let audio = document.getElementById('audio');
         audio.pause();
+    }
+
+    // Function to restart the timer
+    function restartTimer() {
+        stopTimer();
+        startTimer(duration);
     }
 
     // Function to reset the timer display
